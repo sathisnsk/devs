3f64bb35796db59caefed1554da32f75a106723b
sathish <sathis.nsk@gmail.com>
2023-11-17 06:56:39 +0530

    first stable version

--- movieBooking/client/main.js
+++ movieBooking/client/main.js
@@ -1,12 +1,56 @@
 let seatIndex = 0;
-let selectedSeatCount = 0;
-const selectedSeats = [];
+//let selectedSeatCount = 0;
+let selectedSeats = [];
+const moviesAndRates = [{ movieName: "Jailer", rate: 150 }, { movieName: "Leo", rate: 200 }, { movieName: "Thunivu", rate: 250 }];
+let selectedMovie;
+let selectedMovieRate;
 
-function addToLocalStorage() {
+function buildMoviesAndRates() {
+    moviesAndRates.forEach((movieAndRate) => {
+        console.log(movieAndRate.movieName + movieAndRate.rate);
+        const { movieName, rate } = movieAndRate;
+        console.log({ movieName, rate });
+        const selectOption = document.querySelector("#movies");
+        const optionEntry = document.createElement("option");
+        optionEntry.value = movieName;
+        optionEntry.textContent = `${movieName} (Rs.${rate})`;
+        selectOption.appendChild(optionEntry);
+        selectOption.addEventListener("change", changeSelectedMovieAndRate);
+    });
+}
+
+buildMoviesAndRates();
+
+function loadSelectedSeatsAndMovie() {
+    getFromLocalStorage();
+    const selectOption = document.getElementById("movies");
+    selectOption.value = selectedMovie;
+    selectedSeats.forEach((selectedSeat) => {
+        const seatSelected = document.getElementById("seat-" + selectedSeat);
+        console.log(seatSelected);
+        seatSelected.style.backgroundColor = "blue";
+    });
+    buildSummaryValues();
+}
+
+function getFromLocalStorage() {
+    selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    selectedMovieRate = localStorage.getItem("selectedRate");
+    selectedMovie = localStorage.getItem("selectedMovie");
+
+}
+
+function addSeatsToLocalStorage() {
     selectedSeats.sort();
     console.log(selectedSeats);
     localStorage.setItem("selectedSeats", JSON.stringify(selectedSeats));
 }
+
+function addMovieAndRateToLocalStorage() {
+   localStorage.setItem("selectedMovie",selectedMovie);
+   localStorage.setItem("selectedRate",selectedMovieRate);
+}
+
 
 function createSeats(columnName, numberOfSeats) {
     const seatTemplate = document.getElementById("seat-template");
@@ -40,18 +84,45 @@ function seatSelected(seatId, seatIndex) {
         console.log(selectedSeats.find((arrayEntry) => arrayEntry === seatIndex));
 
         console.log(selectedSeats);
-        selectedSeatCount++;
+        // selectedSeatCount++;
         // console.log(buttonStateArray[seatIndex]);
         if (selectedSeats.find((arrayEntry) => arrayEntry === seatIndex) === seatIndex) {
-            console.log("index of value: "+selectedSeats.indexOf(seatIndex));
-            selectedSeats.splice(selectedSeats.indexOf(seatIndex),1);
+            console.log("index of value: " + selectedSeats.indexOf(seatIndex));
+            selectedSeats.splice(selectedSeats.indexOf(seatIndex), 1);
             selectedSeat.style.backgroundColor = "white";
         } else {
             selectedSeat.style.backgroundColor = "blue";
             selectedSeats.push(seatIndex);
         }
-        addToLocalStorage();
+        addSeatsToLocalStorage();
+        buildSummaryValues();
     }
+}
+
+loadSelectedSeatsAndMovie();
 
 
-}    
+
+function buildSummaryValues() {
+
+    const noOfSeats = document.getElementById("no-of-seats");
+    noOfSeats.textContent = selectedSeats.length;
+    /*     const selectValue = document.getElementById("movies");
+        selectValue.value */
+    let totalValue = selectedSeats.length * selectedMovieRate;
+    const totalPrice = document.getElementById("total-price");
+    totalPrice.textContent = totalValue;
+}
+
+
+function changeSelectedMovieAndRate() {
+    const selectValue = document.getElementById("movies");
+    selectedMovie = selectValue.value;
+    const selectedMovieAndRate = moviesAndRates.find((movieAndRate) => {
+        const { movieName, rate } = movieAndRate;
+        return (movieName === selectedMovie)
+    });
+    selectedMovieRate = selectedMovieAndRate.rate;
+    addMovieAndRateToLocalStorage();
+    buildSummaryValues();
+}
